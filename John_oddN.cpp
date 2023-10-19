0a403cbc5cd6feccf83c88eae4e9548ac47d75f5
Gabriel Lebron <102001770+Theking1660@users.noreply.github.com>
2023-10-19 03:26:48 +0000

    0.5

--- John_oddN.cpp
+++ John_oddN.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
 #include <math.h>
+#include <cstdint>
+
 using namespace std;
 
-
-int suma(int veces, int extra)
+string suma(int veces, int extra)
 {
     int valores[5] = {1, 3, 5, 7, 9};
-    int Resultado;
+    long long Resultado=0;
     for (int i = 0; i < veces; i++)
     {
-        Resultado =+ 25;
+        Resultado =Resultado+25;
     }
     for (int i = 0; i < extra; i++)
     {
         Resultado = Resultado + valores[i];
     }
-    return Resultado;
+    return to_string(Resultado);
+
 }
-
-
-
 int main()
 {
-    int numero;
+    double numero;
     cin >> numero;
     int valor_E = numero / 5;
-    double valor_D = round((((numero / 5.0) - valor_E) * 10)/2);
-
-    cout <<suma(valor_E, valor_D) << endl;
+    double valor_D = round((((numero / 5.0) - valor_E) * 10) / 2);
+    cout << suma(valor_E,valor_D)<< endl;
 }
-
