c122362e4b6e5b11ec1a582b5b876eeb82fc576a
Gabriel Lebron <102001770+Theking1660@users.noreply.github.com>
2023-10-19 16:38:16 +0000

    0.6

---
+++ Minimiza_value.py
@@ -0,0 +1,56 @@
+cantidad_N,cantidad_G = list(map(int,input().split()))
+
+def lista_numeros(item):
+    resultados=[]
+    valor_E = int(item/9)
+    valor_D = int((((item/9)-valor_E)*10))
+    for i in range (0,valor_E):
+        resultados.append(1)
+        resultados.append(2)
+        resultados.append(3)
+        resultados.append(4)
+        resultados.append(5)
+        resultados.append(6)
+        resultados.append(7)
+        resultados.append(8)
+        resultados.append(9)
+    
+
+    for i in range (1,valor_D+1):
+        resultados.append(i)
+    return resultados
+
+lista_N=lista_numeros(cantidad_N)
+
+def buscar_Mayor_Caso(lista,cantidad,numeros):
+    lista.sort()
+    Union=[]
+    x = 0
+
+    for i in range(0,numeros):
+        Union.append( [lista[i]])
+    for i in range(0,numeros):
+        lista.pop(0)
+    x+=1
+    while(x<round(cantidad/numeros)):
+        for i in range(0,numeros):
+            Union[i].append( lista[i])
+        for i in range(0,numeros):
+            lista.pop(0)
+        x+=1
+    
+    respuesta =['']
+    Union.sort(reverse=True)
+    for i in range(numeros):
+        for j in Union[i]:
+            respuesta[0]= str(respuesta[0])+ str(j)
+        break
+        
+        
+            
+
+    print(respuesta[0])
+
+
+
+buscar_Mayor_Caso(lista_N,cantidad_N,cantidad_G)      
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Learning_ICPC
 
 Pracicando para el ICPC
+
+Tanto en python como en c++
